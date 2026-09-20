8e78dfc63c8e1295cf958c99ea55a818c0e00cf9
Ali Hasan <hf.alihasan0@gmail.com>
2026-09-20 23:09:58 +0500

    Fix JSX closing tag syntax error in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ function App() {
             <Route path="collaborations" element={<Collaborations />} />
           </Route>
         </Routes>
-      </BrowserRouter>
+      </Router>
     </ThemeProvider>
   )
 }
